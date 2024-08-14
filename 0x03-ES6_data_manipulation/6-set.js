52931d7a1f387fd4caafb420b66704b3be567948
root <mintesinotteferamt@gmail.com>
2024-08-13 19:31:58 -0700

    task7

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Mintesinot Tefera <https://github.com/Mintesinot-Tefera>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
